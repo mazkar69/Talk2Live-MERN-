666a8a9a66f4e7c0e0d2a406305053a174498f76
Mohd Azkar <mohdazkar@outlook.com>
2023-02-26 21:31:00 +0530

    adoptable deployement ready

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -16,7 +16,7 @@ import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
 // const ENDPOINT = "http://localhost:4000"; // "https://talk2live.herokuapp.com"; -> After deployment
-const ENDPOINT = "https://talk2live.herokuapp.com/";
+const ENDPOINT = "https://talk2live.adaptable.app";
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
